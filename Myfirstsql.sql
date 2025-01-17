2e5f40f29a79e8ea6e3638a34c5c22219f34ed26
John Gerega <jjgerega@gmail.com>
2025-01-17 15:08:15 -0500

    Added databases to file

--- Myfirstsql.sql
+++ Myfirstsql.sql
@@ -1,7 +1,11 @@
-create database pelotonknockoff;
+create database vulcanTracker;
 
 show databases;
 
-use pelotonknockoff;
+use vulcanTracker;
 
-create table signup(formno varchar(20), password varchar(20), age varchar(2));
+create table signup(formno varchar(20), fname varchar(20), lname varchar(20), password varchar(20), age varchar(2));
+
+create table login(username varchar(20), password varchar(20));
+
+create table userProfile(username varchar(20), password varchar(20), age varchar(2), miles varchar(9), activites varchar(9));
